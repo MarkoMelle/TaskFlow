e822281ac56e398de594b59be85c77ab202f1be1
Marko Melle <markmelle1@gmail.com>
2025-05-06 22:36:35 +0200

    feat: update site configuration and remove unused comment in index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+   site: 'https://markomelle.github.io/TaskFlow/',
    base: '/TaskFlow/',
    vite: {
      plugins: [tailwindcss()],
